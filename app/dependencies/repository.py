fa33cd5a869c60537d4cf6d32e711735ecef4811
kostin <RocketMenace>
2025-05-29 04:59:37 +0300

    build:
    Implementing get user repository dependency.

--- app/dependencies/repository.py
+++ app/dependencies/repository.py
@@ -4,13 +4,17 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config.database import database
-from app.models.history import RequestHistory
 from app.repository.base import BaseRepositoryProtocol
 from app.repository.history import HistoryRepository
+from app.repository.user import UserRepository
 
+
+async def get_user_repository(
+        session: Annotated[AsyncSession, Depends(database.get_session)]
+) -> BaseRepositoryProtocol:
+    return UserRepository(session=session)
 
 async def get_history_repository(
-    session: Annotated[AsyncSession, Depends(database.get_session)],
-    model: RequestHistory,
+    session: Annotated[AsyncSession, Depends(database.get_session)]
 ) -> BaseRepositoryProtocol:
-    return HistoryRepository(session=session, model=model)
+    return HistoryRepository(session=session)
