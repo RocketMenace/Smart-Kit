6dd8ac0eacbbdb774ed044e457a419f260050be9
RocketMenace <grizzlygit777@gmail.com>
2025-05-29 17:52:42 +0300

    build:
    Implementing get_process_request_use_case dependency.

--- app/dependencies/use_cases.py
+++ app/dependencies/use_cases.py
@@ -9,6 +9,9 @@ from app.use_cases.get_response import SaveResponseUseCase
 from app.use_cases.register_user import UserRegisterUseCase
 from app.services.user import UserService
 from app.dependencies.services import get_user_service
+from app.dependencies.services import get_third_party_service
+from app.services.third_party import ThirdPartyService
+from app.use_cases.process_request import ProcessRequestUseCase
 
 
 async def get_user_register_use_case(
@@ -23,3 +26,8 @@ async def get_save_response_use_case(
     return SaveResponseUseCase(
         history_service=history_service, request_service=request_service
     )
+
+async def get_process_request_use_case(
+        third_party_service: Annotated[ThirdPartyService, Depends(get_third_party_service)]
+) -> ProcessRequestUseCase:
+    return ProcessRequestUseCase(third_party_service=third_party_service)
