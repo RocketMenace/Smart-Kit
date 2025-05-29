8efb574178b37e7e0f3eca0bc72c805f59c37fba
RocketMenace <grizzlygit777@gmail.com>
2025-05-29 17:31:19 +0300

    build:
    Implementing third party server endpoints boilerplate.

---
+++ app/routers/response.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, status, Depends
+from app.schemas.history import RequestInputSchema
+
+router = APIRouter(prefix="/third-party-server", tags=["Response server"])
+
+@router.post("/result", status_code=status.HTTP_200_OK)
+async  def get_response(request: RequestInputSchema):
+    pass
+
+
+@router.get("/ping", status_code=status.HTTP_200_OK)
+async def ping_server():
+    pass
